a58bcddacc35534b523ec3f84589d5b66f8df2b4
Alfred <13776100074@163.com>
2019-12-11 19:35:11 +0800

    create table tbl_role_permission_ref and its relations

---
+++ src/main/java/com/mf/dao/RolePermissionRefMapper.java
@@ -0,0 +1,7 @@
+package com.mf.dao;
+
+import com.mf.core.Mapper;
+import com.mf.model.RolePermissionRef;
+
+public interface RolePermissionRefMapper extends Mapper<RolePermissionRef> {
+}
---
+++ src/main/java/com/mf/web/RolePermissionRefController.java
@@ -0,0 +1,56 @@
+package com.mf.web;
+
+import com.mf.core.Result;
+import com.mf.core.ResultGenerator;
+import com.mf.model.RolePermissionRef;
+import com.mf.service.RolePermissionRefService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+* Created by CodeGenerator on 2019/12/11.
+*/
+@RestController
+@RequestMapping("/role/permission/ref")
+public class RolePermissionRefController {
+
+    @Resource
+    private RolePermissionRefService rolePermissionRefService;
+
+    @PostMapping
+    public Result add(@Validated @RequestBody RolePermissionRef rolePermissionRef) {
+        rolePermissionRefService.save(rolePermissionRef);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Long id) {
+        rolePermissionRefService.deleteByPK(id);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PutMapping
+    public Result update(@Validated @RequestBody RolePermissionRef rolePermissionRef) {
+        rolePermissionRefService.updateByPK(rolePermissionRef);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @GetMapping("/{id}")
+    public Result detail(@PathVariable Long id) {
+        RolePermissionRef rolePermissionRef = rolePermissionRefService.findById(id);
+        return ResultGenerator.genSuccessResult(rolePermissionRef);
+    }
+
+    @GetMapping
+    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        List<RolePermissionRef> list = rolePermissionRefService.findAll();
+        PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+}
