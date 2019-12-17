fe5ec3a5c3e38fb4da646db6b8bb29fc364a0b87
alfred <13776100074@163.com>
2019-12-17 23:06:42 +0800

    软删除统一

--- src/main/java/com/mf/model/RolePermissionRef.java
+++ src/main/java/com/mf/model/RolePermissionRef.java
@@ -1,29 +1,14 @@
 package com.mf.model;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.Date;
 import javax.persistence.*;
 
+@Data
 @Table(name = "tbl_role_permission_ref")
 public class RolePermissionRef extends BaseModal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "created_by")
-    private Long createdBy;
-
-    @Column(name = "created_date")
-    private LocalDate createdDate;
-
-    @Column(name = "last_modified_by")
-    private Long lastModifiedBy;
-
-    @Column(name = "last_modified_date")
-    private LocalDate lastModifiedDate;
-
-    @Column(name = "is_delete")
-    private Boolean isDelete;
 
     @Column(name = "role_id")
     private Long roleId;
