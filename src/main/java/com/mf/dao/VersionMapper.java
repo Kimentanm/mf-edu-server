ecf56933cdade68ec55662c18b6ad9f352a17ae8
alfred <13776100074@163.com>
2019-12-26 23:08:16 +0800

    版本控制基本crud

--- src/main/java/com/mf/dao/VersionMapper.java
+++ src/main/java/com/mf/dao/VersionMapper.java
@@ -2,6 +2,8 @@ package com.mf.dao;
 
 import com.mf.core.Mapper;
 import com.mf.model.Version;
+import org.apache.ibatis.annotations.Param;
 
 public interface VersionMapper extends Mapper<Version> {
+    Version getLastVersionInfo(@Param("type") String type);
 }
