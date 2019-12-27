4db3962403aebf2466b428a6bcb8459543d4f4e8
Kimen <kimen_tang@amaxchina.com>
2019-12-27 09:13:18 +0800

    update version

--- src/main/java/com/mf/model/Version.java
+++ src/main/java/com/mf/model/Version.java
@@ -1,13 +1,13 @@
 package com.mf.model;
 
+import lombok.Data;
+
 import java.util.Date;
 import javax.persistence.*;
 
+@Data
 @Table(name = "tbl_version")
-public class Version {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Version extends BaseModal{
 
     @Column(name = "version_no")
     private Long versionNo;
@@ -20,207 +20,8 @@ public class Version {
 
     private String type;
 
-    @Column(name = "created_by")
-    private Long createdBy;
-
-    @Column(name = "created_date")
-    private Date createdDate;
-
-    @Column(name = "last_modified_by")
-    private Long lastModifiedBy;
-
-    @Column(name = "last_modified_date")
-    private Date lastModifiedDate;
-
-    @Column(name = "is_delete")
-    private Boolean isDelete;
-
     private String description;
-
-    private Integer version;
 
     @Column(name = "update_log")
     private String updateLog;
-
-    /**
-     * @return id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return version_no
-     */
-    public Long getVersionNo() {
-        return versionNo;
-    }
-
-    /**
-     * @param versionNo
-     */
-    public void setVersionNo(Long versionNo) {
-        this.versionNo = versionNo;
-    }
-
-    /**
-     * @return res_url
-     */
-    public String getResUrl() {
-        return resUrl;
-    }
-
-    /**
-     * @param resUrl
-     */
-    public void setResUrl(String resUrl) {
-        this.resUrl = resUrl;
-    }
-
-    /**
-     * @return res_hash
-     */
-    public String getResHash() {
-        return resHash;
-    }
-
-    /**
-     * @param resHash
-     */
-    public void setResHash(String resHash) {
-        this.resHash = resHash;
-    }
-
-    /**
-     * @return type
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * @param type
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    /**
-     * @return created_by
-     */
-    public Long getCreatedBy() {
-        return createdBy;
-    }
-
-    /**
-     * @param createdBy
-     */
-    public void setCreatedBy(Long createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    /**
-     * @return created_date
-     */
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    /**
-     * @param createdDate
-     */
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    /**
-     * @return last_modified_by
-     */
-    public Long getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    /**
-     * @param lastModifiedBy
-     */
-    public void setLastModifiedBy(Long lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    /**
-     * @return last_modified_date
-     */
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    /**
-     * @param lastModifiedDate
-     */
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    /**
-     * @return is_delete
-     */
-    public Boolean getIsDelete() {
-        return isDelete;
-    }
-
-    /**
-     * @param isDelete
-     */
-    public void setIsDelete(Boolean isDelete) {
-        this.isDelete = isDelete;
-    }
-
-    /**
-     * @return description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @param description
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    /**
-     * @return version
-     */
-    public Integer getVersion() {
-        return version;
-    }
-
-    /**
-     * @param version
-     */
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    /**
-     * @return update_log
-     */
-    public String getUpdateLog() {
-        return updateLog;
-    }
-
-    /**
-     * @param updateLog
-     */
-    public void setUpdateLog(String updateLog) {
-        this.updateLog = updateLog;
-    }
 }
