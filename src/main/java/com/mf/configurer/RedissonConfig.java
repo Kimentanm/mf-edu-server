b07a4d5bc8396f71085cfcc5208f4bd0202ecd1c
alfred <13776100074@163.com>
2020-01-05 16:47:30 +0800

    注释redis

--- src/main/java/com/mf/configurer/RedissonConfig.java
+++ src/main/java/com/mf/configurer/RedissonConfig.java
@@ -1,21 +1,21 @@
-package com.mf.configurer;
-
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RedissonConfig {
-
-    @Bean
-    public RedissonClient getRedisson() {
-        Config config = new Config();
-        // 单机模式 依次设置redis地址和密码
-        config.useSingleServer().
-                setAddress("redis://localhost:6379").
-                setPassword("acchow950702");
-        return Redisson.create(config);
-    }
-}
+//package com.mf.configurer;
+//
+//import org.redisson.Redisson;
+//import org.redisson.api.RedissonClient;
+//import org.redisson.config.Config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RedissonConfig {
+//
+//    @Bean
+//    public RedissonClient getRedisson() {
+//        Config config = new Config();
+//        // 单机模式 依次设置redis地址和密码
+//        config.useSingleServer().
+//                setAddress("redis://localhost:6379").
+//                setPassword("acchow950702");
+//        return Redisson.create(config);
+//    }
+//}
