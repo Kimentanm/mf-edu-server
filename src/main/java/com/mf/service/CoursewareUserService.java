bbfb0af9bb608f431d9ce454648d007fef11854b
Alfred <13776100074@163.com>
2020-01-13 20:30:39 +0800

    课件用户关联表（收藏用）

---
+++ src/main/java/com/mf/service/CoursewareUserService.java
@@ -0,0 +1,11 @@
+package com.mf.service;
+
+import com.mf.core.Service;
+import com.mf.model.CoursewareUser;
+
+/**
+ * Created by CodeGenerator on 2020/01/13.
+ */
+public interface CoursewareUserService extends Service<CoursewareUser> {
+
+}
